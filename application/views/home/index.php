6ce4390c4db6657cd8d8e64c8be0aa28e9500638
Micol Peralta <linettekill1@gmail.com>
2019-07-26 01:27:16 -0400

    Creating project

---
+++ application/views/home/index.php
@@ -0,0 +1,94 @@
+<div class="bd-example">
+  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="<?php echo base_url('assets/images/stock/meeting_1.jpg') ?>" class="d-block w-100" height='600px '>
+        <div class="carousel-caption d-none d-md-block">
+          <h3 class="display-3" style="text-shadow: 2px 2px 4px #000000;">First slide label</h3>
+          <p style="text-shadow: 2px 2px 4px #000000;font-size:20px">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="<?php echo base_url('assets/images/stock/meeting_3.jpg') ?>" class="d-block w-100" height='600px '>
+        <div class="carousel-caption d-none d-md-block">
+          <h3 class="display-3" style="text-shadow: 2px 2px 4px #000000;">First slide label</h3>
+          <p style="text-shadow: 2px 2px 4px #000000;font-size:20px">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="<?php echo base_url('assets/images/stock/meeting_2.jpg') ?>" class="d-block w-100" height='600px '>
+        <div class="carousel-caption d-none d-md-block">
+          <h3 class="display-3" style="text-shadow: 2px 2px 4px #000000;">First slide label</h3>
+          <p style="text-shadow: 2px 2px 4px #000000;font-size:20px">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-md-12">
+      <h3 class="text-primary"><i class="fas fa-cube"></i> Lorem ipsum dolor</h3>
+      <hr>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6">
+      <div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <svg class="bd-placeholder-img" width="100%" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96" /><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image</text></svg>
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title text-primary font-weight-bold">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-6">
+      <div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <svg class="bd-placeholder-img" width="100%" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96" /><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image</text></svg>
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title text-primary font-weight-bold">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<div class="container-fluid mb-5 mt-5"> 
+  <div class="row shadow align-items-end" style="background-image: url(<?php echo base_url('assets/images/stock/meeting_1.jpg') ?>);height: 400px;background-repeat: no-repeat;background-size: cover;">
+    <div class="col-md-12">
+      <h3 class="text-white text-center" style="text-shadow: 2px 2px 4px #000000;">Lorem ipsum dolor sit amet.</h3>
+      <p class="text-white text-center" style="text-shadow: 2px 2px 4px #000000;font-size: 18px">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore illum vero labore perferendis laborum aspernatur!</p>
+    </div>
+  </div>
+</div>
---
+++ application/views/home/template.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo $title ?></title>
+	
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/styles.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/fontawesome-all.min.css') ?>">
+
+</head>
+<body>
+	
+	<div class="container-fluid bg-blue">
+		<div class="container">
+			<div class="row p-1 pt-2 pb-2 pl-4 text-white display-7" >
+				<div class="col-lg-3">
+					<i class="fas fa-home mr-1"></i> Direccion Lorem ipsum dolor sit amet.
+				</div>
+				<div class="col-lg-2">
+					<i class="fas fa-phone-alt mr-1"></i> 809-000-0000
+				</div>
+				<div class="col-lg-2">
+					<i class="fas fa-envelope mr-1"></i> correo@correo.com
+				</div>
+				<div class="col-lg-5 text-center">
+					
+					<!-- <a href="" class="mr-4 p-1 " style="background: white"><img src="<?php echo base_url('assets/images/icon/facebook-icon.png') ?>" width='20px' alt=""></a>
+					<a href="" class="mr-4 p-1 " style="background: white"><img src="<?php echo base_url('assets/images/icon/instagram-icon.png') ?>" width='20px' alt=""></a>
+					<a href="" class="mr-4 p-1 " style="background: white"><img src="<?php echo base_url('assets/images/icon/twitter-icon.png') ?>" width='20px' alt=""></a> -->
+					<a href="" class="mr-4"><i class="fab fa-facebook-f"></i></a>
+					<a href="" class="mr-4"><i class="fab fa-twitter"></i></a>
+					<a href="" class="mr-4"><i class="fab fa-instagram"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid">
+		<div class="row align-items-center">
+			<div class="col-md-2 text-center">
+				<img src="<?php echo base_url('assets/images/logo.png') ?>" width='140px' alt="">
+			</div>
+			<div class="col-md-10">
+				
+			</div>
+		</div>
+		<div class="">
+			<div class="row">
+			<!-- <div class="col-md-2 bg-light">
+				<ul class="list-unstyled bg-light">
+				  <li class="bg-light pt-2 pb-2"><a href="">Actividades</a></li>
+				  <li class="bg-light pt-2 pb-2"><a href="">Convenios</a></li>
+				  <li class="bg-light pt-2 pb-2"><a href="">Programas</a></li>
+				</ul>
+			</div> -->
+			<div class="col-md-12 p-0">
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+ 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+    				<span class="navbar-toggler-icon"></span>
+  					</button>
+
+					<div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+					    <ul class="navbar-nav">
+					      <li class="nav-item active mr-3 enlaces-menu">
+					        <a class="nav-link" href="#"><?php echo strtoupper('Nosotros') ?></a>
+					      </li>
+					      <li class="nav-item active mr-3 enlaces-menu">
+					        <a class="nav-link" href="#"><?php echo strtoupper('Servicios') ?></a>
+					      </li>
+					      <li class="nav-item active mr-3 enlaces-menu">
+					        <a class="nav-link" href="#"><?php echo strtoupper('Articulos') ?></a>
+					      </li>
+					      <li class="nav-item active mr-3 enlaces-menu">
+					        <a class="nav-link" href="#"><?php echo strtoupper('Actividades') ?></a>
+					      </li>
+					      <li class="nav-item active mr-3 enlaces-menu">
+					        <a class="nav-link" href="#"><?php echo strtoupper('Donaciones') ?></a>
+					      </li>
+					      <li class="nav-item active mr-3 enlaces-menu">
+					        <a class="nav-link" href="#"><?php echo strtoupper('Programar') ?></a>
+					      </li>
+					      <li class="nav-item active mr-3 dropdown">
+					        <a class="nav-link dropdown-toggle" href="#" id="dropdown08" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo strtoupper('Dropdown') ?></a>
+					        <div class="dropdown-menu" aria-labelledby="dropdown08">
+					          <a class="dropdown-item" href="#">Nosotros</a>
+					          <a class="dropdown-item" href="#">Servicios</a>
+					          <a class="dropdown-item" href="#">Articulos</a>
+					          <a class="dropdown-item" href="#">Actividades</a>
+					          <a class="dropdown-item" href="#">Donaciones</a>
+					          <a class="dropdown-item" href="#">Programar</a>
+					        </div>
+					      </li>
+					    </ul>
+					</div>
+				</nav>
+				<?php $this->load->view($view);?>
+
+			</div>
+		</div>
+		</div>
+	</div>
+
+	<!-- loading view -->
+	<div class="container">
+	</div>
+	
+	<!-- JAVASCRIPT -->
+	<script src="<?php echo base_url('assets/js/jquery_3.4.0.min.js') ?>"></script>
+	<script src="<?php echo base_url('assets/js/jquery.mask.min.js') ?>"></script>
+	<script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+	<script src="<?php echo base_url('assets/js/sweetalert.min.js') ?>"></script>
+	<script src="<?php echo base_url('assets/js/fontawesome-all.min.js') ?>"></script>
+	<script src="<?php echo base_url('assets/js/main.js') ?>"></script>
+
+</body>
+</html>
